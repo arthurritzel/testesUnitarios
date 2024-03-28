bce97d2337186c5e465266b944ba1a1f555aa410
arthurritzel <ritzelarthur@gmail.com>
2024-03-28 15:44:27 -0300

    init

---
+++ src/main/java/br/ce/wcaquino/entidades/Prova.java
@@ -0,0 +1,23 @@
+package br.ce.wcaquino.entidades;
+
+public class Prova {
+
+	private float nota;
+
+	
+		
+	
+	public Prova(float nota) {
+		super();
+		this.nota = nota;
+	}
+
+	public float getNota() {
+		return nota;
+	}
+
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+	
+}
